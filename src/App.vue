8b58e4fcf423af2f2ddfdd5d37d7f3825fcce784
jonburdon <jon@branchoutwebsites.com>
2019-07-25 09:48:52 +0100

    AddTodo functionality added

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="app">
+    <Header />
+    <!-- Use v-on to listen for data and update the dom -->
+    <AddTodo v-on:add-todo="addTodo" />
     <!-- Use v-on to receive id value from del-todo and then call method to delete that todo item -->
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos.vue';
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
+// Don't forget! Each component must be 1) Imported, 2) Listed in export default (below) and 3. included in the html at the top of this file
 export default {
   name: 'app',
   components: {
+    AddTodo,
+    Header,
     Todos
   },
   data () {
@@ -18,16 +26,21 @@ export default {
       todos: [
         {
           id: 1,
-          title: "Become famous",
+          title: "Become famous developer",
           completed: false
         },
           {
           id: 2,
-          title: "Sell film rights to my life",
-          completed: true
+          title: "Spark paradigm shift in society and cultural thinking through use of technology",
+          completed: false
         },
           {
           id: 3,
+          title: "Sell film rights to my life",
+          completed: true
+        },
+        {
+          id: 4,
           title: "Retire and live on luxury yaught",
           completed: false
         }
@@ -38,6 +51,11 @@ export default {
     deleteTodo(id) {
 // Use filter method to select every id in the array apart from that which has been selected by the id clicked.
       this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      // Use spread operator
+      this.todos = [...this.todos, newTodo];
+      this.title = '';
     }
   }
 }
@@ -54,4 +72,18 @@ export default {
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4;
   }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
+  }
+
 </style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <form @submit="addTodo">
+            <!-- Use v-model directive with name so the title field data can be used -->
+            <input type="text" v-model="title" name="title" placeholder="Add Goal ...">
+            <input type="submit" value="Add" class="btn">
+        </form>
+    </div>
+</template>
+
+
+<script>
+import uuid from 'uuid';
+export default {
+    name: "AddTodo",
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        addTodo() {
+            e.preventDefault();
+            const newTodo = {
+                id: uuid.v4(),
+                title: this.title,
+                completed: false
+            }
+            this.$emit('add-todo', newTodo);
+        }
+    }
+}
+</script>
+    name: "AddTodo"
+<style scoped>
+    form {
+        display: flex;
+    }
+
+    input[type="text"] {
+        flex: 10;
+        padding: 5px;
+    }
+
+    input[type="submit"] {
+        flex: 2;
+    }
+</style>
