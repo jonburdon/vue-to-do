f8fe20ed837595c17821ccdd3de44b603bb2d523
jonburdon <jon@branchoutwebsites.com>
2019-07-31 09:37:35 +0100

    Update About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>Example To Do Single Page Application</h1>
-    <p></p>
+    <p>This is the To Do List app v1.0.0</p>
     <p>Built using Vue <a href="https://www.youtube.com/watch?time_continue=9&v=Wy9q22isx3U">Tutorial</a></p>
     <p></p>
   </div>
